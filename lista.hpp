0ee7331c0c5b852e4aaa033be77fd164454da2b2
ThesilentKR <kevinramsesgamer@gmail.com>
2022-11-20 18:31:53 -0600

    Se implemento la estructura de la funcion insertar

--- lista.hpp
+++ lista.hpp
@@ -7,7 +7,7 @@ class Lista{
     class Vertex{
         public:
         
-        Vertex(string pass, string site, string user) { 
+        Vertex(string user, string pass, string site) { 
             passsword = pass; sitio=site; usuario=user;  sig=nullptr; back=nullptr;
         }
 
@@ -26,3 +26,17 @@ class Lista{
     void buscar(string);
     void inicio_de_sesion();
 };
+
+void Lista::insertar(string usuario,string password, string sitio){
+    Vertex *nuevo_nodo = new Vertex(usuario,password,sitio);
+
+    if(head == nullptr){
+        head->back = nuevo_nodo;
+        head=nuevo_nodo;
+        tail=head;
+    }else{
+        tail->sig = nuevo_nodo;
+        nuevo_nodo = nuevo_nodo->back=tail;
+        tail=nuevo_nodo;
+    }
+}
